7a75d3ffce9f8055e4dfc2c36675805db7c01bd0
Victor Augusto <vaas@icomp.ufam.edu.br>
2021-12-11 11:03:06 -0400

    WIP

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import AddIcon from "@mui/icons-material/Add";
-import { Fab } from "@mui/material";
+import { Fab, Paper, useTheme } from "@mui/material";
 import { Box, BoxProps } from "@mui/system";
+import { motion } from "framer-motion";
 import { useLayoutContext } from "../../contexts/LayoutContext";
 import { DialogAddTask } from "../DialogAddTask";
 import { Header } from "../Header";
@@ -16,26 +17,56 @@ export const Layout: React.FC<LayoutProps> = ({
   childrenBoxProps,
   ...props
 }) => {
-  const { setOpenDialogNewTask } = useLayoutContext();
+  const { setOpenDialogNewTask, openMenu } = useLayoutContext();
+  const theme = useTheme();
+  const layoutVariant = {
+    hidden: { scale: 1, x: 0 },
+    visible: { scale: 0.85, x: "70%" },
+  };
+  const paperVariant = {
+    hidden: { borderRadius: 0 },
+    visible: { borderRadius: (theme.shape.borderRadius as number) * 8 },
+  };
 
   return (
-    <Box {...props}>
-      <Header />
+    <Box sx={{ backgroundColor: "#020417" }}>
+      {/* @ts-ignore */}
       <Box
-        {...childrenBoxProps}
-        sx={{ px: 2, ...childrenBoxProps?.sx, position: "relative" }}
+        {...props}
+        component={motion.div}
+        sx={{ overflow: "hidden" }}
+        variants={layoutVariant}
+        animate={openMenu ? "visible" : "hidden"}
+        initial="hidden"
+        transition={{ duration: 1 }}
       >
-        {children}
-        <Box sx={{ position: "fixed", bottom: "2rem", right: "2rem" }}>
-          <Fab
-            onClick={() => setOpenDialogNewTask(true)}
-            color="secondary"
-            aria-label="add"
+        <Paper
+          sx={{
+            backgroundColor: (theme) => theme.palette.background.default,
+            minHeight: "100vh",
+          }}
+          component={motion.div}
+          variants={paperVariant}
+          transition={{ duration: 1 }}
+        >
+          <Header />
+          <Box
+            {...childrenBoxProps}
+            sx={{ px: 2, ...childrenBoxProps?.sx, position: "relative" }}
           >
-            <AddIcon />
-          </Fab>
-        </Box>
-        <DialogAddTask />
+            {children}
+            <Box sx={{ position: "fixed", bottom: "2rem", right: "2rem" }}>
+              <Fab
+                onClick={() => setOpenDialogNewTask(true)}
+                color="secondary"
+                aria-label="add"
+              >
+                <AddIcon />
+              </Fab>
+            </Box>
+            <DialogAddTask />
+          </Box>
+        </Paper>
       </Box>
     </Box>
   );
